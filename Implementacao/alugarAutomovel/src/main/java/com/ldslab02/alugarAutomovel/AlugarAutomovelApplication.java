22c09fb92277e80bec589809d40b8c25bf9b88a1
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-09-09 10:55:50 -0300

    create maven project with initial dependencies

---
+++ Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/AlugarAutomovelApplication.java
@@ -0,0 +1,13 @@
+package com.ldslab02.alugarAutomovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugarAutomovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugarAutomovelApplication.class, args);
+	}
+
+}
---
+++ Implementacao/alugarAutomovel/src/test/java/com/ldslab02/alugarAutomovel/AlugarAutomovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldslab02.alugarAutomovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugarAutomovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
