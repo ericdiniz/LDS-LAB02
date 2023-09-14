3e37d2b6da649e5be45916a754e4e7fe72643624
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-09-14 17:56:30 -0300

    update customer class

--- Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/models/Customer.java
+++ Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/models/Customer.java
@@ -2,8 +2,12 @@ package com.ldslab02.alugarAutomovel.models;
 
 import java.util.List;
 
+import org.hibernate.annotations.Cascade;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -74,10 +78,16 @@ public class Customer extends User {
     @Size(groups = CreateCustomer.class, min = 2, max = 100)
     private String profession;
 
-    @Column(name = "income", unique = true, length = 100, nullable = false)
+    @Column(name = "income", unique = true, nullable = false)
     @NotNull(groups = CreateCustomer.class)
     @NotEmpty(groups = CreateCustomer.class)
-    @Size(groups = CreateCustomer.class, min = 2, max = 100)
+    @Size(groups = CreateCustomer.class, min = 0)
+    @ElementCollection(fetch = FetchType.LAZY, targetClass = Double.class)
+    @CollectionTable(name = "Incomes", // Nome da tabela para armazenar a coleção
+            joinColumns = @JoinColumn(name = "customer_id") // Nome da coluna que fará referência à entidade principal
+                                                            // (Customer)
+    )
+    @Cascade(org.hibernate.annotations.CascadeType.ALL) // Configura o CascadeType.ALL
     private List<Double> income;
 
     @OneToOne(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
