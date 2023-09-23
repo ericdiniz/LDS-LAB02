19b6f293904e1898480e15835d5c12149ed4adfa
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-09-22 23:35:12 -0300

    created vehicles service

---
+++ Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/services/VehiclesService.java
@@ -0,0 +1,53 @@
+package com.ldslab02.alugarAutomovel.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ldslab02.alugarAutomovel.models.Vehicles;
+import com.ldslab02.alugarAutomovel.repositories.VehiclesRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class VehiclesService {
+
+    @Autowired
+    private VehiclesRepository vehiclesRepository;
+
+    public Vehicles findbyIdVehicle(Long id) {
+        Optional<Vehicles> vehicle = this.vehiclesRepository.findById(id);
+        return vehicle.orElseThrow(
+                () -> new RuntimeException("Veículo não encontrado" + id + "Tipo: " + Vehicles.class.getName()));
+    }
+
+    @Transactional
+    public Vehicles createVehicles(Vehicles obj) {
+        obj.setId(null);
+        obj = this.vehiclesRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Vehicles updateVehicles(Vehicles obj, boolean blocked) {
+        Vehicles newObj = findbyIdVehicle(obj.getId());
+        newObj.setMatricula(obj.getMatricula());
+        newObj.setYear(obj.getYear());
+        newObj.setBrand(obj.getBrand());
+        newObj.setModel(obj.getModel());
+        newObj.setNumberPlate(obj.getNumberPlate());
+        newObj.setBlocked(blocked);
+        return this.vehiclesRepository.save(newObj);
+    }
+
+    public void deleteUser(Long id) {
+        findbyIdVehicle(id);
+        try {
+            this.vehiclesRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+
+}
