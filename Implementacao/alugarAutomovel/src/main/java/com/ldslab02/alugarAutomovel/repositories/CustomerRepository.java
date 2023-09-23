a58ff9d9c26670a636fecc5ba1d8d1de8bf212ed
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-09-22 23:35:51 -0300

    annotation @Repository

--- Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/repositories/CustomerRepository.java
+++ Implementacao/alugarAutomovel/src/main/java/com/ldslab02/alugarAutomovel/repositories/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.ldslab02.alugarAutomovel.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ldslab02.alugarAutomovel.models.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
